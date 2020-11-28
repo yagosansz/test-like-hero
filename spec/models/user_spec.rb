040e110ba93bc710544afbc4de27a8e5dbdfb0c8
Yago Santos <yagosansz@gmail.com>
2020-11-28 16:10:03 -0500

    Added tests to User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid if the level is not between 1 and 99" do
+    hero = User.create(kind: :wizard, nickname: "My Hero", level: 100)
+    expect(hero).to_not be_valid
+  end
+
+  it "returns the correct hero title" do
+    hero = User.create(kind: :wizard, nickname: "My Hero", level: 25)
+    expect(hero.title).to eq "wizard My Hero #25"
+  end
 end
