e31a871ed6217af3c7048c6f7196cee5c427bbaf
Yago Santos <yagosansz@gmail.com>
2020-11-28 16:35:58 -0500

    Improved tests by adding gem ffaker

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
-    hero = User.create(kind: :wizard, nickname: "My Hero", level: 100)
-    expect(hero).to_not be_valid
+    kind = %i(wizard knight).sample
+    nickname = FFaker::Name.first_name
+    level = FFaker::Random.rand(100..999)
+
+    user = User.new(kind: kind, nickname: nickname, level: level)
+    expect(user).to_not be_valid
   end
 
   it "returns the correct hero title" do
-    hero = User.create(kind: :wizard, nickname: "My Hero", level: 25)
-    expect(hero.title).to eq "wizard My Hero #25"
+    kind = %i(wizard knight).sample
+    nickname = FFaker::Name.first_name
+    level = FFaker::Random.rand(1..99)
+
+    user = User.new(kind: kind, nickname: nickname, level: level)
+    expect(user.title).to eq "#{kind} #{nickname} ##{level}"
   end
 end
